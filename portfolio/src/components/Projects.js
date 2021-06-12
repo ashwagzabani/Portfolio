4ba75adc628d35c723cdeddabcfd09b91f335369
ashwag zabani <ashwag.amz@gmail.com>
2021-06-13 00:13:53 +0300

    finish projects component

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -11,23 +11,23 @@ class Projects extends Component {
             {
                 logo: teach_me_logo,
                 name: 'TeachMe',
-                description: '',
+                description: 'Deployed this project using: HTML, CSS, Bootstrap, Python, Django, and Postgres. It is a website allowed the instructor to create a new course and add their content, and allowed the learner to enrollment to these courses and download download course attachment files',
                 github: 'https://github.com/ashwagzabani/TeachMe',
                 link: ''
             },
             {
                 logo: blogy_logo,
                 name: 'Blogy',
-                description: 'Blogy It is an online diary or journal located that shows you a list of posts in different categories you can filter the post by any category you want. Also, you can create your own account and shares your opinions and ideas in your specialty or anything you want with others. In addition, you can engage with others posts by add comments or like their posts.Users cannot post anything without admin permission to publish and users can report any inappropriate content so Blogy Admin can check and solved it.Implementation using HTML, CSS, Bootstrap, Python, Ajax, Django, and Postgres.                ',
+                description: 'Deployed this project using: HTML, CSS, Bootstrap, Python, Django, and Postgres. It is a website where a writer share their views on an individual subject. It allows the logged-in user to create their own post, interact with published posts by sharing their opinion and report a post',
                 github: 'https://github.com/ashwagzabani/Blogy',
-                link: ''
+                link: 'https://blogyproject.herokuapp.com/'
             },
             {
                 logo: logo,
                 name: 'Mini-Twitter',
-                description: '',
-                github: '',
-                link: ''
+                description: 'Deployed this project using: HTML, CSS, React Js, React Bootstrap and Axsios. This project allowed the user to add new tweets and managed them using their account',
+                github: 'https://github.com/ashwagzabani/Mini-Twitter',
+                link: 'https://mini-twitter-react-app.herokuapp.com/'
             }
         ]
 
@@ -35,25 +35,29 @@ class Projects extends Component {
             return (
                 <div className="card">
                     <div className="row">
-                        <div className="col-md-3">
+                        <div className="col-md-3 logo">
                             <img src={project.logo} alt={project.logo} width="80px" />
                         </div>
-                        <div className="col-md-9">
-                            <h5>{project.name}</h5>
-                            <p>{project.description}</p>
-                            <a>Github Link</a>
+                        <div className="col-md-9 details">
+                            <div className="project-details">
+                                {project.name == 'TeachMe' ?
+                                    <h5>{project.name}</h5>
+                                    :
+                                    <h5><a href={project.link} target="_blanck">{project.name}</a></h5>}
 
-                            {project.name == 'TeachMe' ?
-                                <p>Websit Link not aveilabe yet</p>
-                                :
-                                <a href={project.link}>Websit Link</a>}
+                                <a href={project.github} target="_blanck"><i className="fa fa-github"></i></a>
+                            </div>
+                            <p>{project.description}</p>
+                            {/* <a>Github Link</a> */}
+
+
                         </div>
                     </div>
                 </div>
             );
         })
         return (
-            <div className="container projects">
+            <div className="container projects overflow-auto">
                 <h1>Projects</h1>
                 {viewMyProjects}
             </div>
